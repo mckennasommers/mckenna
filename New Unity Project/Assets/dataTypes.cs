fd779b691379dfe5cf0292597910016120980426
McKenna Sommers <mckenna.sommers@gmail.com>
2016-03-02 08:53:21 -0700

    Data Types

---
+++ New Unity Project/Assets/dataTypes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dataTypes : MonoBehaviour {
+
+	void Start () 
+	{
+		//value
+		Vector3 pos = transform.position;
+		pos = new Vector3(0, 2, 0);
+
+		//Reference
+		Transform tran = transform;
+		tran.position = new Vector3(0, 2, 0);
+	}
+}
