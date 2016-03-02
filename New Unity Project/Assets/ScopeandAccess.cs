49dbe40e1129edb2bb690b53a0da1152c4c2fccd
McKenna Sommers <mckenna.sommers@gmail.com>
2016-03-02 08:44:18 -0700

    Scope and Access

---
+++ New Unity Project/Assets/ScopeandAccess.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeandAccess : MonoBehaviour {
+
+	public int one = 7;
+
+
+	private int two = 3;
+	private int three = 8;
+
+
+	private anotherClass myOtherClass;
+
+
+	void Start ()
+	{
+		one = 12;
+
+		myOtherClass = new anotherClass();
+		myOtherClass.Shape(one, myOtherClass.square);
+	}
+
+
+	void Example (int red, int blue)
+	{
+		int answer;
+		answer = red * blue * one;
+		Debug.Log(answer);
+	}
+
+
+	void Update ()
+	{
+		Debug.Log("One is set to: " + one);
+	}
+}
---
+++ New Unity Project/Assets/anotherClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anotherClass : MonoBehaviour {
+
+	public int square;
+	public int circle;
+
+
+	private int pencil;
+	private int paper;
+
+
+	public void Shape (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Shape total: " + answer);
+	}
+
+
+	private void OfficeSort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Office Supplies total: " + answer);
+	}
+}
